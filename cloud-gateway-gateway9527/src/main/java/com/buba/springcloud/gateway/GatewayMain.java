172c14f852308d92c771da8fc5c30344cfbc106f
bin <bin211314>
2023-01-06 14:52:56 +0800

    cloud代码首次提交

---
+++ cloud-gateway-gateway9527/src/main/java/com/buba/springcloud/gateway/GatewayMain.java
@@ -0,0 +1,23 @@
+package com.buba.springcloud.gateway;
+
+
+import com.buba.springcloud.gateway.filter.TokenFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain.class,args);
+    }
+
+
+    @Bean
+    public TokenFilter tokenFilter(){
+        return new TokenFilter();
+    }
+
+}
